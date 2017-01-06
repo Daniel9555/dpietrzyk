58d9c243dd09b231132fd97e20e202c2d282ede3
Daniel9555 <dpietrzyk@sigma.ug.edu.pl>
2017-01-06 15:44:50 +0100

    Update Lab4.c

--- Labolatorium1/Lab4.c
+++ Labolatorium1/Lab4.c
@@ -1,52 +1,48 @@
 #include <stdio.h>
-//zad1
+
+
 int main() {
+
    int a;
-   printf("Podaj pierwsza liczbe calkowita: \n");
+   int i;
+   printf("Zad1\n Podaj pierwsza liczbe calkowita: \n");
    scanf("%d", &a);
    int b;
   printf("Podaj druga liczbe calkowita: \n");
    scanf("%d", &b);
-   for (int i=a; i<=b; i++) {
+   for (i=a; i<=b; i++) {
    printf("%d\n" ,i);
 }
- 
-
-   //zad 2
-       int c;
-       printf("Podaj pierwsza liczbe calkowita \n");
-       scanf("%d",&c) ;
-       int b;
-       printf("Podaj druga liczbe calkowita \n");
-       scanf("%d",&b);
-       int suma = 0;
-       for (int  a = c; a <= b; a++) {
-       suma = suma + a;
+    int c;
+    printf("Zad2\n Podaj pierwsza liczbe calkowita \n");
+    scanf("%d",&c) ;
+    printf("Podaj druga liczbe calkowita \n");
+    scanf("%d",&b);
+    int suma = 0;
+    for (a = c; a <= b; a++) {
+    suma = suma + a;
    }
-       printf("%d",suma);
+    printf("%d",suma);
 
-   //zad 3
-      int x;
-      int sum = 0;
-      int wantContinue;
-      printf("Czy kontynuowac (1-tak/0-nie)? \n");
-      scanf("%d",&wantContinue);
 
-      while (wantContinue == 1) {
-          printf("Podaj liczbe : \n");
-          scanf("%d",&x);
-          sum += x*x;
-          printf("Czy kontynuowac (1-tak/0-nie)? \n");
-          scanf("%d",&wantContinue);
+    int x;
+    int sum = 0;
+    int wantContinue;
+    printf("\nZad3\n Czy kontynuowac (1-tak/0-nie)? \n");
+    scanf("%d",&wantContinue);
+
+
+    while (wantContinue == 1) {
+        printf("Podaj liczbe : \n");
+        scanf("%d",&x);
+        sum += x*x;
+        printf("Czy kontynuowac (1-tak/0-nie)? \n");
+        scanf("%d",&wantContinue);
+
       }
       printf("%d\n",sum);
-      
-
-   //zad 4
-      int a;
-      int i;
       int silnia = 1;
-      printf("Podaj liczbe calkowita \n");
+      printf("Zad4\n Podaj liczbe calkowita \n");
       scanf("%d",&a);
       for(i = a; i>1; i--) {
       silnia *=i;
