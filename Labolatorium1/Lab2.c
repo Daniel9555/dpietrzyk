ea657a027a168f377c6ac4f5dbe8810df77b9271
Daniel9555 <dpietrzyk@sigma.ug.edu.pl>
2017-01-06 15:06:14 +0100

    Update Lab2.c

--- Labolatorium1/Lab2.c
+++ Labolatorium1/Lab2.c
@@ -1,96 +1,58 @@
 #include <stdio.h>
 
-//Zad1
 int main() {
-float a;
-printf("%f\n",a);
-scanf("%f",&a);
- 
-float b;
-printf("%f\n",b);
-scanf("%f",&b);
- 
-float c;
-printf("%f\n",c);
-scanf("%f",&c);
-  
-float wynik = (a*b) - (b/c)
-printf("%d\n" ,wynik);
-}
 
-//Zad2
-int main() {
 int a;
-printf("%f\n",a);
-scanf("%f",&a);
- 
-double b;
-printf("%d\n",b);
-scanf("%d",&b);
+scanf("%d", &a);
+int b;
+scanf("%d", &b);
+int c;
+scanf("%d", &c);
 
-double c;
-printf("%d\n",c);
-scanf("%d",&c);
-  
-double wynik = (a*b) - (b%c)
-printf("%d\n" ,wynik);
-}
+double dzialanie1 = a * b - ((double) b / c );
+printf("%f\n", dzialanie1 );
 
-//Zad3
-int main() {
-int a;
-printf("%f\n",a);
-scanf("%f",&a);
- 
-double b;
-printf("%d\n",b);
-scanf("%d",&b);
 
-double c;
-printf("%d\n",c);
-scanf("%d",&c);
-  
-double wynik = (a*b) - (c*a) - (b*c)
-printf("%d\n" ,wynik);
-}
+double dzialanie2 = a*b - b%c;
+printf("%f\n", dzialanie2 );
 
-//Zad4
-int main() {
-  int a;
-  printf("%d\n",a);
-  scanf("%d",&a);
+double dzialanie3 = a*b - c*a - b*c;
+printf("%f\n", dzialanie3 );
 
-  float b;
-  printf("%f\n",b);
-  scanf("%f",&b);
+printf("%d, %d, %d\n",a,b,c );
 
-  float c;
-  printf("%f\n",c);
-  scanf("%f",&c);
-  int a,b,c;
+
+
+
   if ((a>b) && (b>c) && (a>c)) {
+
   printf("%d, %d, %d\n" ,c,b,a);
+
 } else if((b>a) && (a>c) && (b>c)) {
+
   printf("%d, %d, %d\n" ,c,a,b);
+
 } else if((c>a) && (b>a) && (c>b)) {
+
   printf("%d, %d, %d\n" ,a,b,c);
+
 } else if((a>b) && (b<c) && (a>c)) {
+
   printf("%d, %d, %d\n" ,b,c,a);
+
 } else if((b>a) && (a<c) && (b>c)) {
+
   printf("%d, %d, %d\n" ,a,c,b);
+
 } else if((c>a) && (b<a) && (c>b)) {
+
   printf("%d, %d, %d\n" ,b,a,c);
+
   } else {
+
+
 
   }
 
-
-  return 0;
+return 0;
 }
-
-
-
-
-
-
-
